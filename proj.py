d55c4dadaeafbf5f714283a9b6d1952a7ae7b742
David Matthews <dmatthe1@uvm.edu>
2021-05-13 14:43:51 -0400

    wip on proj

--- proj.py
+++ proj.py
@@ -9,6 +9,7 @@ from scipy.spatial import KDTree
 
 from data_utils import *
 from haversine import dist
+import mapclassify
 
 df = load_business_data()
 census_df = pd.read_csv("merged_census_with_centroid.csv")
@@ -16,8 +17,11 @@ census_df = pd.read_csv("merged_census_with_centroid.csv")
 census_tracts_df = gpd.read_file("zip://../tmp/census/2010/Tract_2010Census_DP1.zip")
 census_tracts_df["Centroid_Lng"] = census_tracts_df["geometry"].apply(lambda x: x.centroid.x)
 census_tracts_df["Centroid_Lat"] = census_tracts_df["geometry"].apply(lambda x: x.centroid.y)
-census_tracts_df = census_tracts_df[["GEOID10", "ALAND10", "Centroid_Lat", "Centroid_Lng", "DP0010001"]]
+# census_tracts_df = census_tracts_df[["GEOID10", "ALAND10", "Centroid_Lat", "Centroid_Lng", "DP0010001"]]
 census_tracts_df.rename(columns={"DP0010001":"POP10"}, inplace=True)
+
+# census_df_poverty = pd.read_csv("../tmp/nhgis0001_csv/nhgis0001_ds177_20105_2010_tract.csv")
+census_df_income = pd.read_csv("../tmp/nhgis0001_csv/nhgis0001_ds176_20105_2010_tract.csv")
 
 categories = np.array(sorted([s.lower() for s in df["fldHeading"].unique()]))
 
@@ -86,7 +90,22 @@ for category, df_category in [("fresh_food", df_fresh_food_stores),
 census_df_with_people["PopDensity"] = census_df_with_people["POP10"] / (census_df_with_people["ALAND10"]/(1000000) + 1e-3) # min ALAND > 0 is 1. convert to people / km^2
 census_df_with_people_tract["PopDensity"] = census_df_with_people_tract["POP10"] / (census_df_with_people_tract["ALAND10"]/(1000000) + 1e-3) # min ALAND > 0 is 1. convert to people / km^2
 
+# VT: 50
+# CA: 08
+census_df_with_people_tract[census_df_with_people_tract["GEOID10"].str.startswith("50")].plot(column='fresh_food', scheme='quantiles', k=5, figsize=(16, 9), legend=True)
+census_df_with_people_tract[census_df_with_people_tract["GEOID10"].str.startswith("50")].plot(column='restaurants', scheme='quantiles', k=5, figsize=(16, 9), legend=True)
+census_df_with_people_tract[census_df_with_people_tract["GEOID10"].str.startswith("50")].plot(column='convenience_store', scheme='quantiles', k=5, figsize=(16, 9), legend=True)
+census_df_with_people_tract[census_df_with_people_tract["GEOID10"].str.startswith("50")].plot(column='PopDensity', scheme='quantiles', k=5, figsize=(16, 9), legend=True)
 
+census_df_with_people_tract[census_df_with_people_tract["GEOID10"].str.startswith("25")].plot(column='fresh_food', scheme='quantiles', k=5, figsize=(16, 9), legend=True)
+census_df_with_people_tract[census_df_with_people_tract["GEOID10"].str.startswith("25")].plot(column='restaurants', scheme='quantiles', k=5, figsize=(16, 9), legend=True)
+census_df_with_people_tract[census_df_with_people_tract["GEOID10"].str.startswith("25")].plot(column='convenience_store', scheme='quantiles', k=5, figsize=(16, 9), legend=True)
+census_df_with_people_tract[census_df_with_people_tract["GEOID10"].str.startswith("25")].plot(column='PopDensity', scheme='quantiles', k=5, figsize=(16, 9), legend=True)
+
+census_df_with_people_tract[census_df_with_people_tract["GEOID10"].str.startswith("36")].plot(column='fresh_food', scheme='quantiles', k=5, figsize=(16, 9), legend=True)
+census_df_with_people_tract[census_df_with_people_tract["GEOID10"].str.startswith("36")].plot(column='restaurants', scheme='quantiles', k=5, figsize=(16, 9), legend=True)
+census_df_with_people_tract[census_df_with_people_tract["GEOID10"].str.startswith("36")].plot(column='convenience_store', scheme='quantiles', k=5, figsize=(16, 9), legend=True)
+census_df_with_people_tract[census_df_with_people_tract["GEOID10"].str.startswith("36")].plot(column='PopDensity', scheme='quantiles', k=5, figsize=(16, 9), legend=True)
 
 for category in ["fresh_food", "restaurants", "convenience_store"]:
     plt.scatter(census_df_with_people_tract[f"{category}"].values,
@@ -104,6 +123,14 @@ plt.show()
 log_pop_density = np.log10(census_df_with_people_tract["PopDensity"].values)
 log_food_dist = np.log10(census_df_with_people_tract["fresh_food"].values)
 slope, intercept, rval, pval, stderr = scipy.stats.linregress(log_food_dist, log_pop_density)
+slope, rval
+
+log_restaurants_dist = np.log10(census_df_with_people_tract["restaurants"].values)
+slope, intercept, rval, pval, stderr = scipy.stats.linregress(log_restaurants_dist, log_pop_density)
+slope, rval
+
+log_convenience_store_dist = np.log10(census_df_with_people_tract["convenience_store"].values)
+slope, intercept, rval, pval, stderr = scipy.stats.linregress(log_convenience_store_dist, log_pop_density)
 slope, rval
 
 log_pop_density = np.log10(census_df_with_people_tract["restaurants"].values)
